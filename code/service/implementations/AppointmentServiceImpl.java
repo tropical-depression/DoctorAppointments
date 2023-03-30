2bbf87a27ac6d8935f24e443cf70cdab69d43110
Nedeljko Kuzmanovic <n.kuzmanovic@vegait.rs>
2023-03-30 10:54:59 +0200

    Inverted if statement logic to worse solution

--- code/service/implementations/AppointmentServiceImpl.java
+++ code/service/implementations/AppointmentServiceImpl.java
@@ -34,85 +34,86 @@ public class AppointmentServiceImpl implements AppointmentService {
 	public void publishAppointmentNotes(UUID id) throws ResourceNotFoundException {
 		AppointmentNotes an = appointmentRepository.getById(id);
 
-		if (an == null) {
+		if (an != null) {
+			Prescription p = prescriptionRepository.getByAppointmentId(id);
+			Referral r = referralRepository.getByAppointmentId(id);
+			FitNote fn = fitNoteRepository.getByAppointmentId(id);
+
+			Date appointmentTime = an.getAppointmentTime();
+			String patientName = an.getPatientName();
+			String deliveryAddress = r != null && r.getType() == ReferralType.IN_PERSON_GP_APPOINTMENT
+					&& r.isCoveredByInsurance() ? r.getPracticeAddress() : an.getPatientAddress();
+
+			BigDecimal price = new BigDecimal(50.0);
+
+			if (p != null) {
+				price = price.add(Prescription.STANDARD_PRESCRIPTION_PRICE);
+			}
+
+			if (r != null && !r.isCoveredByInsurance() && r.getPrice().compareTo(BigDecimal.ZERO) > 0) {
+				price = price.add(r.getPrice());
+			}
+
+			AppointmentInvoice ai = new AppointmentInvoice(id, price, appointmentTime, patientName, deliveryAddress);
+
+			ArrayList<Document> list = new ArrayList<Document>();
+
+			if (an != null) {
+				Document appointmentNotesPdf = documentRepository.uploadAppointmentNotesPdf(an,
+						"/pdfTemplates/appointmentNotes.pdftemplate");
+				list.add(appointmentNotesPdf);
+			}
+
+			if (p != null) {
+				String prescriptionPdfTemplatePath = "";
+				switch (p.getType()) {
+				case ELECTRONIC:
+					prescriptionPdfTemplatePath = "/pdfTemplates/prescription/electronic.pdftemplate";
+					break;
+				case FAX:
+					prescriptionPdfTemplatePath = "/pdfTemplates/prescription/fax.pdftemplate";
+					break;
+				case DELIVERY:
+					prescriptionPdfTemplatePath = "/pdfTemplates/prescription/delivery.pdftemplate";
+					break;
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + p.getType());
+				}
+				Document prescriptionPdf = documentRepository.uploadPrescriptionPdf(p, prescriptionPdfTemplatePath);
+				list.add(prescriptionPdf);
+			}
+
+			if (r != null) {
+				String referralPdfTemplatePath = "";
+				switch (r.getType()) {
+				case IN_PERSON_GP_APPOINTMENT:
+					referralPdfTemplatePath = "/pdfTemplates/referrals/inPerson";
+					break;
+				case SPECIALIST_APPOINTMENT:
+					referralPdfTemplatePath = "/pdfTemplates/referrals/specialist";
+					break;
+				default:
+					throw new IllegalArgumentException("Unexpected value: " + r.getType());
+				}
+				Document referralPdf = documentRepository.uploadReferralPdf(r, referralPdfTemplatePath);
+				list.add(referralPdf);
+			}
+
+			if (fn != null) {
+				Document fitNotePdf = documentRepository.uploadFitNotePdf(fn, "pdfTemplates/fitNote.pdftemplate");
+				list.add(fitNotePdf);
+			}
+
+			Document appointmentInvoiceDocument = documentRepository.uploadAppointmentInvoicePdf(ai,
+					AppointmentInvoice.PDF_TEMPLATE_PATH);
+			list.add(appointmentInvoiceDocument);
+
+			an.setAppointmentDocuments(list);
+
+			appointmentRepository.update(an);
+		}
+		else {
 			throw new ResourceNotFoundException("Appointment with provided id has not been found");
 		}
-
-		Prescription p = prescriptionRepository.getByAppointmentId(id);
-		Referral r = referralRepository.getByAppointmentId(id);
-		FitNote fn = fitNoteRepository.getByAppointmentId(id);
-
-		Date appointmentTime = an.getAppointmentTime();
-		String patientName = an.getPatientName();
-		String deliveryAddress = r != null && r.getType() == ReferralType.IN_PERSON_GP_APPOINTMENT
-				&& r.isCoveredByInsurance() ? r.getPracticeAddress() : an.getPatientAddress();
-
-		BigDecimal price = new BigDecimal(50.0);
-
-		if (p != null) {
-			price = price.add(Prescription.STANDARD_PRESCRIPTION_PRICE);
-		}
-
-		if (r != null && !r.isCoveredByInsurance() && r.getPrice().compareTo(BigDecimal.ZERO) > 0) {
-			price = price.add(r.getPrice());
-		}
-
-		AppointmentInvoice ai = new AppointmentInvoice(id, price, appointmentTime, patientName, deliveryAddress);
-
-		ArrayList<Document> list = new ArrayList<Document>();
-
-		if (an != null) {
-			Document appointmentNotesPdf = documentRepository.uploadAppointmentNotesPdf(an,
-					"/pdfTemplates/appointmentNotes.pdftemplate");
-			list.add(appointmentNotesPdf);
-		}
-
-		if (p != null) {
-			String prescriptionPdfTemplatePath = "";
-			switch (p.getType()) {
-			case ELECTRONIC:
-				prescriptionPdfTemplatePath = "/pdfTemplates/prescription/electronic.pdftemplate";
-				break;
-			case FAX:
-				prescriptionPdfTemplatePath = "/pdfTemplates/prescription/fax.pdftemplate";
-				break;
-			case DELIVERY:
-				prescriptionPdfTemplatePath = "/pdfTemplates/prescription/delivery.pdftemplate";
-				break;
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + p.getType());
-			}
-			Document prescriptionPdf = documentRepository.uploadPrescriptionPdf(p, prescriptionPdfTemplatePath);
-			list.add(prescriptionPdf);
-		}
-
-		if (r != null) {
-			String referralPdfTemplatePath = "";
-			switch (r.getType()) {
-			case IN_PERSON_GP_APPOINTMENT:
-				referralPdfTemplatePath = "/pdfTemplates/referrals/inPerson";
-				break;
-			case SPECIALIST_APPOINTMENT:
-				referralPdfTemplatePath = "/pdfTemplates/referrals/specialist";
-				break;
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + r.getType());
-			}
-			Document referralPdf = documentRepository.uploadReferralPdf(r, referralPdfTemplatePath);
-			list.add(referralPdf);
-		}
-
-		if (fn != null) {
-			Document fitNotePdf = documentRepository.uploadFitNotePdf(fn, "pdfTemplates/fitNote.pdftemplate");
-			list.add(fitNotePdf);
-		}
-
-		Document appointmentInvoiceDocument = documentRepository.uploadAppointmentInvoicePdf(ai,
-				AppointmentInvoice.PDF_TEMPLATE_PATH);
-		list.add(appointmentInvoiceDocument);
-
-		an.setAppointmentDocuments(list);
-
-		appointmentRepository.update(an);
 	}
 }
